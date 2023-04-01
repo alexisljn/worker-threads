11b822213543f44b06dd23ef0d99ef70690bc4b9
alexisljn <alexis.lejeune@gmx.fr>
2023-04-01 11:44:26 +0200

    use worker in dedicated route

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+import {Worker} from "worker_threads";
+
 const express = require('express')
 const app = express()
 const port = 3000
@@ -13,6 +15,18 @@ app.get('/block', (req: Request, res: Response) => {
 
     res.json({count});
 })
+
+app.get('/worker', (req: Request, res: Response) => {
+    // This resolves file path of the worker in working and dist directories
+    const workerPath = require.resolve('./workers/loop');
+
+    const worker = new Worker(workerPath);
+
+    worker.on('message', ({count}) => {
+        res.json({count});
+    })
+})
+
 
 app.get('/ping', (req: Request, res: Response) => {
     res.send('pong');
