f70bd39567fe1cf3c33b4f60cba21eb02eb55331
alexisljn <alexis.lejeune@gmx.fr>
2023-04-01 11:48:55 +0200

    add comments at top of routes

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,9 @@ const app = express()
 const port = 3000
 import {Request, Response} from "express";
 
+/*
+* This route prevents other users to make a request to the server until the for loop has completed its cycle
+*/
 app.get('/block', (req: Request, res: Response) => {
     let count: number = 0;
 
@@ -14,8 +17,12 @@ app.get('/block', (req: Request, res: Response) => {
     }
 
     res.json({count});
-})
+});
 
+/*
+* This route allows other users to make request to the server because now the for loop iteration has been deported
+* to a worker thread
+ */
 app.get('/worker', (req: Request, res: Response) => {
     // This resolves file path of the worker in working and dist directories
     const workerPath = require.resolve('./workers/loop');
@@ -24,14 +31,19 @@ app.get('/worker', (req: Request, res: Response) => {
 
     worker.on('message', ({count}) => {
         res.json({count});
-    })
-})
+    });
+});
 
 
+/*
+* Simple route to call while /block or /worker are called
+* /block case : response will be sent after the for loop iteration has completed
+* /worker case : response will be sent immediately after calling /ping
+ */
 app.get('/ping', (req: Request, res: Response) => {
     res.send('pong');
-})
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+});
